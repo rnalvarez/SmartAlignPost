2f8675572b7f70c31598067c8ce06c36cbe9dd87
Ramiro N. Alvarez <64320107+rnalvarez@users.noreply.github.com>
2026-09-13 18:21:36 -0300

    Agregar editor VSTGUI propio al Controller

--- src/controller.h
+++ src/controller.h
@@ -1,11 +1,17 @@
 #pragma once
 #include "public.sdk/source/vst/vsteditcontroller.h"
 #include "plugin_ids.h"
+#include "pluginterfaces/vst/ivstplugview.h"
 
 namespace SmartAlignPost {
 class Controller final : public Steinberg::Vst::EditController {
 public:
     static Steinberg::FUnknown* createInstance(void*);
     Steinberg::tresult PLUGIN_API initialize(Steinberg::FUnknown* context) SMTG_OVERRIDE;
+    Steinberg::IPlugView* PLUGIN_API createView(const char* name) SMTG_OVERRIDE;
+    Steinberg::tresult PLUGIN_API getParamStringByValue(
+        Steinberg::Vst::ParamID tag,
+        Steinberg::Vst::ParamValue valueNormalized,
+        Steinberg::Vst::String128 string) SMTG_OVERRIDE;
 };
 }
