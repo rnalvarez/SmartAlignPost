8ac035806a9df383b07483f8b90456ed850890e3
Ramiro N. Alvarez <64320107+rnalvarez@users.noreply.github.com>
2026-09-20 17:25:40 -0300

    feat: define residual VST parameter ids

--- src/plugin_ids.h
+++ src/plugin_ids.h
@@ -1,7 +1,13 @@
 #pragma once
 #include "pluginterfaces/base/funknown.h"
+#include "pluginterfaces/vst/vsttypes.h"
 
 namespace SmartAlignPost {
+
 static const Steinberg::FUID ProcessorUID (0x6D1A92B1, 0x3D724E41, 0xA81C6E20, 0x71F9D532);
 static const Steinberg::FUID ControllerUID(0xD81C5F70, 0x9B3142A6, 0xC6E1A1D4, 0x4E5B9F03);
+
+static constexpr Steinberg::Vst::ParamID kResidualSamplesParam = 1001;
+static constexpr Steinberg::Vst::ParamID kApplyResidualParam = 1002;
+
 }
